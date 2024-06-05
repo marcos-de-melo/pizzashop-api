7899cb59be3929d22d630cf7b052a74fed2df903
Marcos de Melo <marcdmelo@gmail.com>
2024-06-05 16:34:50 -0300

    Listagem de pedidos

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,48 +1,48 @@
-import { Elysia } from 'elysia'
-import { cors } from '@elysiajs/cors'
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-import { registerRestaurant } from './routes/register-restaurant'
-import { registerCustomer } from './routes/register-customer'
-import { sendAuthenticationLink } from './routes/send-authentication-link'
-import { createOrder } from './routes/create-order'
-import { approveOrder } from './routes/approve-order'
-import { cancelOrder } from './routes/cancel-order'
-import { getOrders } from './routes/get-orders'
-import { createEvaluation } from './routes/create-evaluation'
-import { getEvaluations } from './routes/get-evaluations'
-import { updateMenu } from './routes/update-menu'
-import { updateProfile } from './routes/update-profile'
-import { authentication } from './authentication'
-import { getProfile } from './routes/get-profile'
-import { authenticateFromLink } from './routes/authenticate-from-link'
-import { getManagedRestaurant } from './routes/get-managed-restaurant'
-import { signOut } from './routes/sign-out'
-import { getOrderDetails } from './routes/get-order-details'
-import { getMonthReceipt } from './routes/get-month-receipt'
-import { getMonthOrdersAmount } from './routes/get-month-orders-amount'
-import { getDayOrdersAmount } from './routes/get-day-orders-amount'
-import { getMonthCanceledOrdersAmount } from './routes/get-month-canceled-orders-amount'
-import { getDailyReceiptInPeriod } from './routes/get-daily-receipt-in-period'
-import { getPopularProducts } from './routes/get-popular-products'
-import { dispatchOrder } from './routes/dispatch-order'
-import { deliverOrder } from './routes/deliver-order'
+import { registerRestaurant } from "./routes/register-restaurant";
+import { registerCustomer } from "./routes/register-customer";
+import { sendAuthenticationLink } from "./routes/send-authentication-link";
+import { createOrder } from "./routes/create-order";
+import { approveOrder } from "./routes/approve-order";
+import { cancelOrder } from "./routes/cancel-order";
+import { getOrders } from "./routes/get-orders";
+import { createEvaluation } from "./routes/create-evaluation";
+import { getEvaluations } from "./routes/get-evaluations";
+import { updateMenu } from "./routes/update-menu";
+import { updateProfile } from "./routes/update-profile";
+import { authentication } from "./authentication";
+import { getProfile } from "./routes/get-profile";
+import { authenticateFromLink } from "./routes/authenticate-from-link";
+import { getManagedRestaurant } from "./routes/get-managed-restaurant";
+import { signOut } from "./routes/sign-out";
+import { getOrderDetails } from "./routes/get-order-details";
+import { getMonthReceipt } from "./routes/get-month-receipt";
+import { getMonthOrdersAmount } from "./routes/get-month-orders-amount";
+import { getDayOrdersAmount } from "./routes/get-day-orders-amount";
+import { getMonthCanceledOrdersAmount } from "./routes/get-month-canceled-orders-amount";
+import { getDailyReceiptInPeriod } from "./routes/get-daily-receipt-in-period";
+import { getPopularProducts } from "./routes/get-popular-products";
+import { dispatchOrder } from "./routes/dispatch-order";
+import { deliverOrder } from "./routes/deliver-order";
 
 const app = new Elysia()
   .use(
     cors({
       credentials: true,
-      allowedHeaders: ['content-type'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+      allowedHeaders: ["content-type"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
       origin: (request): boolean => {
-        const origin = request.headers.get('origin')
+        const origin = request.headers.get("origin");
 
         if (!origin) {
-          return false
+          return false;
         }
 
-        return true
+        return true;
       },
-    }),
+    })
   )
   .use(authentication)
   .use(signOut)
@@ -71,24 +71,24 @@ const app = new Elysia()
   .use(getPopularProducts)
   .onError(({ code, error, set }) => {
     switch (code) {
-      case 'VALIDATION': {
-        set.status = error.status
+      case "VALIDATION": {
+        set.status = error.status;
 
-        return error.toResponse()
+        return error.toResponse();
       }
-      case 'NOT_FOUND': {
-        return new Response(null, { status: 404 })
+      case "NOT_FOUND": {
+        return new Response(null, { status: 404 });
       }
       default: {
-        console.error(error)
+        console.error(error);
 
-        return new Response(null, { status: 500 })
+        return new Response(null, { status: 500 });
       }
     }
-  })
+  });
 
-app.listen(3333)
+app.listen(3333);
 
 console.log(
-  `🔥 HTTP server running at ${app.server?.hostname}:${app.server?.port}`,
-)
+  `🔥 HTTP server running at ${app.server?.hostname}:${app.server?.port}`
+);
